0712d2bdc1a4cf49e9f1d566db1df7fc64c2eacd
agustin <agustinjalilleon@gmail.com>
2026-07-18 15:23:16 -0300

    chore: sync 2026-07-18 · 15:22:53

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { CustomerProvider } from '@/context/customer-context';
+import { TrpcProvider }    from '@/lib/trpc/provider';
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
@@ -23,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans antialiased`}>
         <Header />
-        {children}
+        <CustomerProvider><TrpcProvider>{children}</TrpcProvider></CustomerProvider>
         <Analytics />
         <Footer />
       </body>
